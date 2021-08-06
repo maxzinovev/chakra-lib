8144839f0c1908e19d1031c8a825f514674bcf83
maxzinovev <max@zinovev.com>
2021-08-06 14:31:47 +0300

    рефакторинг

--- dist/FormLabel/FormLabel.d.ts
+++
@@ -1,1 +0,0 @@
-export declare const FormLabel: ({ children, name, ...props }: any) => JSX.Element;
--- dist/index.js
+++
@@ -1,8 +0,0 @@
-
-'use strict'
-
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./chakra-lib.cjs.production.min.js')
-} else {
-  module.exports = require('./chakra-lib.cjs.development.js')
-}
